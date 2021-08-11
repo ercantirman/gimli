efbec2ea13a61a793660a53ba4c442cfc3ab1b21
Ercan Tırman <ercantirman@gmail.com>
2021-08-11 15:25:47 +0300

    adedsome comments on Cloth entity

--- Gimli/Data/Entities/Cloth.cs
+++ Gimli/Data/Entities/Cloth.cs
@@ -8,5 +8,10 @@ namespace Gimli.Data.Entities
         public string Name { get; set; }
 
         public ClothType Type { get; set; }
+
+        //We have forgotten the ImageUrl field before
+        //Don't forget to add a new migration and update the database after modifying this entity like below
+
+        //public string ImageUrl { get; set; }
     }
 }
